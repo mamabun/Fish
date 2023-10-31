7fabd611c2046cecedb43b0665319b87f02e5719
badag <badag@DESKTOP-MAMABUN>
2023-10-30 22:20:12 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        Label1 = New Label()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(15, 115)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(85, 32)
+        Button1.TabIndex = 0
+        Button1.Text = "ok"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(129, 115)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(85, 32)
+        Button2.TabIndex = 1
+        Button2.Text = "ok"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(243, 115)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(85, 32)
+        Button3.TabIndex = 2
+        Button3.Text = "dont"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = My.Resources.Resources.tumblr_b22e49d8f52d0e90857458021399339d_7a9859cc_1280
+        PictureBox1.Location = New Point(3, 0)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(62, 58)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 3
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = My.Resources.Resources.fish_water_png_3928
+        PictureBox2.Location = New Point(106, 11)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(222, 84)
+        PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox2.TabIndex = 4
+        PictureBox2.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(71, 11)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(29, 15)
+        Label1.TabIndex = 5
+        Label1.Text = "fiish"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(354, 155)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox1)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Form1"
+        ShowIcon = False
+        Text = "WARNING"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Diagnostics
+Imports System.IO
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim proc = Process.GetProcessesByName("Fish")
+        For i As Integer = 0 To proc.Count - 1
+            proc(i).CloseMainWindow()
+        Next i
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim myProcess = New Process()
+        myProcess.StartInfo.UseShellExecute = True
+        myProcess.StartInfo.FileName = "https://www.youtube.com/watch?v=sdYjJsr6FS8"
+        myProcess.Start()
+        Me.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Process.Start("Fish.exe")
+    End Sub
+End Class
